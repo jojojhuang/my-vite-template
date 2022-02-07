c58f15d2880cd090a651f5c7db9623d88d1a640f
jojojhuang <jojojhuang.git@gmail.com>
2022-02-08 07:25:36 +0800

    [chore] update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,16 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['plugin:vue/vue3-essential', 'standard', 'plugin:prettier/recommended'],
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:prettier/recommended'
+  ],
   parserOptions: {
     ecmaVersion: 13,
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
-  plugins: ['vue', '@typescript-eslint', 'prettier'],
-  rules: {}
+  plugins: ['vue', '@typescript-eslint', 'prettier']
 }
