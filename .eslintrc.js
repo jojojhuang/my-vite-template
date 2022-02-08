0b6462205181772bb3c00d07009e33c39ec18371
jojojhuang <jojojhuang.git@gmail.com>
2022-02-08 19:26:02 +0800

    [chore] update

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,34 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
-  plugins: ['vue', '@typescript-eslint', 'prettier']
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: ['variable', 'function'],
+        format: ['camelCase'],
+        leadingUnderscore: 'allow'
+      },
+      {
+        selector: ['class', 'interface'],
+        format: ['PascalCase']
+      },
+      {
+        selector: ['enumMember'],
+        format: ['UPPER_CASE'],
+        leadingUnderscore: 'allow'
+      }
+    ],
+    '@typescript-eslint/prefer-for-of': 'warn',
+    '@typescript-eslint/array-type': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'vue/component-definition-name-casing': ['warn', 'PascalCase'],
+    'vue/no-use-v-if-with-v-for': 'error',
+    'vue/prop-name-casing': 'warn',
+    'vue/custom-event-name-casing': 'warn',
+    eqeqeq: 'off',
+    'vue/eqeqeq': ['error', 'always'],
+    '@typescript-eslint/no-unnecessary-type-constraint': 'warn'
+  }
 }
